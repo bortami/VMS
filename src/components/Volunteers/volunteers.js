bd75a69442d94603462d8e799ac1c90ff808ee81
Michelle Tabor <mtabor66@outlook.com>
2019-03-28 22:53:23 -0400

    added table and buttons

---
+++ src/components/Volunteers/volunteers.js
@@ -0,0 +1,127 @@
+import React, { Component } from "react";
+import {
+  Box,
+  Button,
+  CheckBox,
+  Table,
+  TextInput,
+  TableHeader,
+  TableRow,
+  TableCell,
+  TableBody,
+  Heading
+} from "grommet";
+import { Search, Checkbox } from "grommet-icons";
+
+export default class Volunteers extends Component {
+  state = {
+    search: "",
+    volunteers: [
+      {
+        name: "Sally",
+        rating: 2,
+        dateRegistered: "May 3, 2019",
+        hoursLogged: 72,
+        isActive: true
+      },
+      {
+        name: "Sally",
+        rating: 2,
+        dateRegistered: "May 3, 2019",
+        hoursLogged: 72,
+        isActive: true
+      },
+      {
+        name: "Sally",
+        rating: 2,
+        dateRegistered: "May 3, 2019",
+        hoursLogged: 72,
+        isActive: true
+      },
+      {
+        name: "Sally",
+        rating: 2,
+        dateRegistered: "May 3, 2019",
+        hoursLogged: 72,
+        isActive: true
+      }
+    ]
+  };
+  handleFieldChange = e => {
+    const stateToChange = {};
+    stateToChange[e.target.id] = e.target.value;
+    this.setState(stateToChange);
+  };
+  render() {
+    return (
+      <Box
+        direction="column"
+        pad="medium"
+        basis="full"
+        fill
+        align="center"
+        flex
+        overflow={{ horizontal: "hidden" }}
+      >
+        <Box direction="row" pad="medium" width="50vw" align="center">
+          <TextInput
+            placeholder="Search by Name or email address"
+            onChange={e => this.handleFieldChange(e)}
+            id="search"
+            name="search-input"
+          />
+          <Button icon={<Search />} label="Search" onClick={() => {}} />
+        </Box>
+        <Box direction="row" pad="mediium" align="center">
+          <Heading level={4} pad="medium">
+            Quick Search:{" "}
+          </Heading>
+          <Button label="Top Hours" />
+          <Button label="Top Rated" />
+          <Button label="Newest" />
+        </Box>
+        <Box elevation="medium" pad="medium">
+          Volunteer Results
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableCell scope="col">
+                  <CheckBox />
+                </TableCell>
+                <TableCell scope="col">Name</TableCell>
+                <TableCell scope="col">Rating</TableCell>
+                <TableCell scope="col">Date Joined</TableCell>
+                <TableCell scope="col">Hours Logged</TableCell>
+                <TableCell scope="col">Active Status</TableCell>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {this.state.volunteers.map(volunteer => {
+                return (
+                  <TableRow>
+                    <TableCell scope="row">
+                      <Checkbox />
+                    </TableCell>
+                    <TableCell>{volunteer.name}</TableCell>
+                    <TableCell>{volunteer.rating}</TableCell>
+                    <TableCell>{volunteer.dateRegistered}</TableCell>
+                    <TableCell>{volunteer.hoursLogged}</TableCell>
+                    <TableCell>
+                      {volunteer.isActive ? "Active" : "Inactive"}
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+          <Box direction="row" pad="mediium" align="center">
+            <Button label="Add to Project" />
+            <Button label="Email" />
+            <Button label="Add to Group" />
+            <Button label="Generate List" />
+          </Box>
+        </Box>
+      </Box>
+    );
+  }
+}
