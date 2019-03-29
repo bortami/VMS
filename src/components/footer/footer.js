f6b288315fb88fd7d7b4179bfb6590bd63a087c1
Michelle Tabor <mtabor66@outlook.com>
2019-03-28 21:49:53 -0400

    fixed styling

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,43 +1,37 @@
-import React, { Component } from 'react';
-import { Box, Image, Anchor, Paragraph } from 'grommet';
-import logo from '../../images/VMS.png';
+import React, { Component } from "react";
+import { Box, Image, Anchor, Paragraph } from "grommet";
+import logo from "../../images/VMS.png";
 
-const FootBox = (props)=>(
-	<Box
-		tag="footer"
-		direction="row"
-		align="center"
-		justify="between"
-		background="light"
-		pad={{ left: 'small', right: 'small', vertical: 'small' }}
-		elevation="none"
-		style={{ zIndex: '1' }}
-		{...props}
-	/>
+const FootBox = props => (
+  <Box
+    tag="footer"
+    direction="row"
+    align="center"
+    justify="between"
+    background="light"
+    pad={{ left: "small", right: "small", vertical: "small" , top: "0", bottom: "0"}}
+    elevation="small"
+  
+    {...props}
+  />
 );
 export default class Footer extends Component {
-    
-	render() {
-		return (
-			<FootBox>
-				<Box direction="column">
-					<Anchor href="/help">Help</Anchor>
-					<Anchor href="/help/faq">FAQs</Anchor>
-					<Anchor href="/help/contact">Contact Us</Anchor>
-					<Anchor href="/help/tc">Terms and Conditions</Anchor>
-				</Box>
-				<Box direction="row">
-					<Box width="200" height="75px">
-						<Image fit="contain" src={logo} />
-					</Box>
-					<Box>
-						<Paragraph size="small">
-							9 Any Street<br />Any Town, WV, USA
-						</Paragraph>
-						<Anchor href="mailto:email@email.com">supportemail@email.co</Anchor>
-					</Box>
-				</Box>
-			</FootBox>
-		);
-	}
+  render() {
+    return (
+      <FootBox>
+       
+          <Box height="50px">
+            <Image fit="contain" src={logo} />
+          </Box>
+          <Box pad="small" direction="row" flex justify="between" align="center">
+            <Paragraph size="small">9 Any Street, Any Town, WV, USA</Paragraph>
+         
+            <Anchor href="mailto:email@email.com">supportemail@email.co</Anchor>
+         
+          <Anchor href="/help">Help</Anchor>
+        </Box>
+		
+      </FootBox>
+    );
+  }
 }
