ec8319c2da16a0dfaa871429abf5645025569e0c
Michelle Tabor <mtabor66@outlook.com>
2019-04-04 10:46:56 -0400

    skills now rendering

--- src/components/Users/userSetting.js
+++ src/components/Users/userSetting.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Box, Heading, Text, Button, Paragraph, Form, FormField, TextInput } from 'grommet';
 import { Close } from 'grommet-icons';
+import api from '../../modules/apiManager';
 
 export default class UserSettings extends Component {
 	state = {
@@ -8,19 +9,35 @@ export default class UserSettings extends Component {
 		newPassword: '',
 		confirmNewPassword: '',
 		username: '',
-		errorMessage: ''
+		errorMessage: '',
+		skills: []
 	};
 	handlePasswordChange = (e) => {
 		e.preventDefault();
 	};
 	handleUsernameChange = (e) => {
 		e.preventDefault();
+		const newUsername = {
+			username: this.state.username
+		};
+		return null;
 	};
 	handleFieldChange = (evt) => {
 		const stateToChange = {};
 		stateToChange[evt.target.id] = evt.target.value;
 		this.setState(stateToChange);
 	};
+	componentDidMount() {
+		const newState = {};
+		api.all('skills').then((skills) => {
+			newState.skills = skills;
+			api.single('organizations', sessionStorage.getItem('userId')).then((user) => {
+				newState.oldPassword = user.password;
+				this.setState(newState);
+			});
+		});
+	}
+
 	render() {
 		return (
 			<Box pad="medium" direction="row" overflow="scroll" fill justify="center">
@@ -36,7 +53,7 @@ export default class UserSettings extends Component {
 					<Heading level={3}>Settings</Heading>
 					<Text size="medium">Edit your Settings</Text>
 
-					<Heading level={5}>Change your password</Heading>
+					<Heading level={4}>Change your password</Heading>
 					<Form onSubmit={this.handlePasswordChange}>
 						<FormField label="Old Password" htmlFor="oldPassword">
 							<TextInput required id="oldPassword" name="oldPassword" onChange={this.handleFieldChange} />
@@ -70,6 +87,19 @@ export default class UserSettings extends Component {
 						</FormField>
 						<Button type="submit" label="Update Organization's Name" />
 					</Form>
+
+					<Heading level={4}>Edit your Organization's Skills</Heading>
+					<Form onSubmit={this.handleFieldChange}>
+						<FormField label="New Skill" htmlFor="skills">
+							<TextInput id="skills" name="skills" onChage={this.handleFieldChange} />
+						</FormField>
+						<Button type="submit" label="Add Skill" />
+					</Form>
+					<Box>
+						{this.state.skills.map((skill) => (
+							<Button onClick={this.doathingwithaSkll} type="button" label={skill.name} />
+						))}
+					</Box>
 				</Box>
 
 				<Box>
--- src/components/landingPage/landingPage.js
+++ src/components/landingPage/landingPage.js
@@ -9,33 +9,7 @@ export default class LandingPage extends Component {
   state = {
     search: "",
     projects: [
-      {
-        id: 1,
-        name: "Sample Name One",
-        location: "Sample, WV",
-        date: new Date(),
-        host: "Alliance for Alliances",
-        shortDescription:
-          "Think tank impact effective altruism natural resources move the needle academic, to agile inspirational leverage collaborate LGBTQ+. Social impact; leverage her body hze. Scale and impact living a fully ethical life scalable parse radical shared unit of analysis scalable radical unprecedented challenge resist inclusion. Deep dive, communities, shared value social entrepreneurship; collaborate do-gooder, strategize and a, circular white paper dynamic cultivate."
-      },
-      {
-        id: 2,
-        name: "Sample Name Two",
-        location: "Sample, WV",
-        date: new Date(),
-        host: "Alliance for Alliances",
-        shortDescription:
-          "Think tank impact effective altruism natural resources move the needle academic, to agile inspirational leverage collaborate LGBTQ+. Social impact; leverage her body her rights big data targeted black lives matter indicators low-hanging fruit. Uplift big data a; external partners "
-      },
-      {
-        id: 3,
-        name: "Sample Name Three",
-        location: "Sample, WV",
-        date: new Date(),
-        host: "Alliance for Alliances",
-        shortDescription:
-          "Replicable impact investing do-gooder engaging social enterprise. Corporate social responsibility collective impact data agile strategize strategize. Scale and impact living a fully ethical life scalable parse radical shared unit of analysis scalable radical unprecedented challenge resist inclusion. Deep dive, communities, shared value social entrepreneurship; collaborate do-gooder, strategize and a, circular white paper dynamic cultivate."
-      }
+      
     ]
   };
   handleFieldChange = e => {
@@ -63,31 +37,7 @@ export default class LandingPage extends Component {
           />
           <Button icon={<Search />} label="Search" onClick={() => {}} />
         </Box>
-        <Box direction="row" pad="medium" height="auto">
-          {this.state.projects.map(project => {
-            return (
-              <Box elevation="medium" pad="0" key={project.id} margin="small">
-                <Box pad="0" margin="10px">
-                  <Heading level={4} color="basic" margin="none">{project.name}</Heading>
-				  <Heading level={6} margin="none">{project.location}</Heading>
-				  <Anchor href="/organization/profile">{project.host}</Anchor>
-                </Box>
-                {/* <Heading level={6}>{project.location}</Heading>
-							 */}
-                <Box overflow="scroll" pad="small">
-                  <Paragraph size="small" pad="small">
-                    {project.shortDescription}
-                  </Paragraph>
-                </Box>
-                <Box alignSelf="end" align="end" pad="small" margin="xsmall">
-                  <Button icon={<View/>} margin="small" type="button" label="view more">
-                   
-                  </Button>
-                </Box>
-              </Box>
-            );
-          })}
-        </Box>
+        
         <Box direction="row" pad="medium">
           <Heading alignSelf="center" level={1}>
             Something Inspirational
