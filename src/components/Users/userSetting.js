8b7746d39f8544fbb40fda8b95245db737eecc82
Michelle Tabor <mtabor66@outlook.com>
2019-04-04 12:03:53 -0400

    can now add skills

--- src/components/Users/userSetting.js
+++ src/components/Users/userSetting.js
@@ -6,22 +6,76 @@ import api from '../../modules/apiManager';
 export default class UserSettings extends Component {
 	state = {
 		oldPassword: '',
+		oldPasswordCheck: '',
 		newPassword: '',
 		confirmNewPassword: '',
 		username: '',
 		errorMessage: '',
-		skills: []
+		skills: [],
+		newSkill: '',
+		successMessage: ''
 	};
 	handlePasswordChange = (e) => {
 		e.preventDefault();
+		if (this.state.oldPassword !== this.state.oldPasswordCheck) {
+			const errorMessage = 'Your old password is incorrect';
+			this.setState({ errorMessage: { passwordError: errorMessage } });
+			return null;
+		} else if (this.state.newPassword !== this.state.confirmNewPassword) {
+			const errorMessage = 'Your new passwords do not match.';
+			this.setState({ errorMessage: { passwordError: errorMessage } });
+			return null;
+		} else if (this.state.newPassword === this.state.oldPassword) {
+			const errorMessage = 'Your old and new password cannot be the same.';
+			this.setState({ errorMessage: { passwordError: errorMessage } });
+			return null;
+		}
+		const newPassword = {
+			id: sessionStorage.getItem('userId'),
+			password: this.state.newPassword
+		};
+		api.put('organizations', newPassword).then(
+			() => document.querySelector('#password-form').reset(),
+			this.setState({
+				successMessage: { passwordSuccess: 'Password has been changed' },
+				errorMessage: { passwordError: '' }
+			})
+		);
 	};
 	handleUsernameChange = (e) => {
 		e.preventDefault();
 		const newUsername = {
+			id: sessionStorage.getItem('userId'),
 			username: this.state.username
 		};
-		return null;
+		api
+			.put('organizations', newUsername)
+			.then(
+				() => document.querySelector('#username-form').reset(),
+				this.setState({ successMessage: { usernameSuccess: 'Username has been changed' } })
+			);
 	};
+	addNewSkill = (e) => {
+		e.preventDefault();
+		this.state.skills.map((skill) => {
+			if (skill.name === this.state.newSkill) {
+				debugger;
+				this.setState({ errorMessage: { skillError: 'Skill already exists' } });
+				return null;
+			} else {
+				const newSkill = {
+					name: this.state.newSkill
+				};
+				return api.post(newSkill, 'skills').then(() => {
+					api.all('skills').then((skills) => {
+						this.setState({ skills: skills });
+						document.querySelector('#skills-form').reset();
+					});
+				});
+			}
+		});
+	};
+
 	handleFieldChange = (evt) => {
 		const stateToChange = {};
 		stateToChange[evt.target.id] = evt.target.value;
@@ -54,9 +108,14 @@ export default class UserSettings extends Component {
 					<Text size="medium">Edit your Settings</Text>
 
 					<Heading level={4}>Change your password</Heading>
-					<Form onSubmit={this.handlePasswordChange}>
-						<FormField label="Old Password" htmlFor="oldPassword">
-							<TextInput required id="oldPassword" name="oldPassword" onChange={this.handleFieldChange} />
+					<Form onSubmit={this.handlePasswordChange} id="password-form">
+						<FormField label="Old Password" htmlFor="oldPasswordCheck">
+							<TextInput
+								required
+								id="oldPasswordCheck"
+								name="oldPasswordCheck"
+								onChange={this.handleFieldChange}
+							/>
 						</FormField>
 						<FormField label="New Password" htmlFor="newPassword">
 							<TextInput required id="newPassword" name="newPassword" onChange={this.handleFieldChange} />
@@ -70,32 +129,31 @@ export default class UserSettings extends Component {
 							/>
 						</FormField>
 						<Button type="submit" label="Update Password" primary />
+						<Paragraph color="green">{this.state.successMessage.passwordSuccess}</Paragraph>
+						<Paragraph color="red">{this.state.errorMessage.passwordError}</Paragraph>
 					</Form>
 
 					<Heading level={4}>Change your username</Heading>
-					<Form onSubmit={this.handleUsernameChange}>
+					<Form onSubmit={this.handleUsernameChange} id="username-form">
 						<FormField label="New Username" htmlFor="username">
 							<TextInput required id="username" name="username" onChange={this.handleFieldChange} />
 						</FormField>
 						<Button type="submit" label="Update Username" />
-					</Form>
-
-					<Heading level={4}>Change your Organization's Name</Heading>
-					<Form onSubmit={this.handleNameChange}>
-						<FormField label="name" htmlFor="name">
-							<TextInput required id="name" name="name" onChange={this.handleFieldChange} />
-						</FormField>
-						<Button type="submit" label="Update Organization's Name" />
+						<Paragraph color="green">{this.state.successMessage.usernameSuccess}</Paragraph>
 					</Form>
 
 					<Heading level={4}>Edit your Organization's Skills</Heading>
-					<Form onSubmit={this.handleFieldChange}>
-						<FormField label="New Skill" htmlFor="skills">
-							<TextInput id="skills" name="skills" onChage={this.handleFieldChange} />
+					<Form onSubmit={this.addNewSkill} id="skills-form">
+						<FormField label="New Skill" htmlFor="newSkill">
+							<TextInput id="newSkill" name="newSkill" onChange={this.handleFieldChange} />
 						</FormField>
 						<Button type="submit" label="Add Skill" />
+						<Paragraph color="red">{this.state.errorMessage.skillError}</Paragraph>
 					</Form>
 					<Box>
+						<Text size="xsmall" color="red">
+							Deleting skills cannot be undone and can have unintended consequences!
+						</Text>
 						{this.state.skills.map((skill) => (
 							<Button onClick={this.doathingwithaSkll} type="button" label={skill.name} />
 						))}
